cb67eaf283993f5ab2f055b6bd1df02a74558cc0
Adrian-Garcia <agarc5@hotmail.com>
2019-08-14 17:35:45 -0500

    changed the title of page for now

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+       Random Nasa Photo Generator!
       </p>
       <CardContainer />
     </div>
