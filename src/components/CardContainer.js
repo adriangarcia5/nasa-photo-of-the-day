a6d2da2d0ccdd002514d5aa40cbd9a0805565dcb
Adrian-Garcia <agarc5@hotmail.com>
2019-08-14 17:32:36 -0500

    added axios that i forgot to add at beginning

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,32 +1,37 @@
-
 // Building out the NASA card that will show an image
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import NasaCard from "./NasaCard.js";
-import { className } from "postcss-selector-parser";
+import randomDate from "./randomDate.js";
 
- function CardContainer() {
-    function CardContainer() {
-        // NASA KEY: https://api.nasa.gov/planetary/apod?api_key=JmvQUr9DIoMy0TsbX9mx0ZbTpbVnMDiWhUdFBCIg
-        const [container, setContainer] = useState([])
+function CardContainer() {
+    // NASA KEY: https://api.nasa.gov/planetary/apod?api_key=JmvQUr9DIoMy0TsbX9mx0ZbTpbVnMDiWhUdFBCIg
+    const [nasaData, setNasaData] = useState([])
+    const [date, setDate] = useState('2012-3-14')
+    let start = new Date(2001, 0, 1)
+    let end = new Date()
     
-         useEffect(() => {
-            axios
-                .get(`https://api.nasa.gov/planetary/apod?api_key=JmvQUr9DIoMy0TsbX9mx0ZbTpbVnMDiWhUdFBCIg&date=2012-03-14`)
-                .then(response => {
-                    const cards = response.data.url;
-                    console.log('NASA API Response: ', cards)
-                    setContainer(cards)
-                })
-                .catch(error => {
-                    console.log('ERROR ==> ', error)
-                })
-        }, [])
+    useEffect(() => {
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=JmvQUr9DIoMy0TsbX9mx0ZbTpbVnMDiWhUdFBCIg&date=${date}`)
+            .then(response => {
+                const cards = response.data;
+                console.log('NASA API Response: ', cards)
+                setNasaData(cards)
+            })
+            .catch(error => {
+                console.log('ERROR ==> ', error)
+            })
+    }, [date])
 
-        return(
-            <div className="card-container">
-                <NasaCard imgUrl={container}/>
-            </div>
-        )}
- }
+    return (
+        <div className="card-container">
+            <button onClick={() => setDate(`${randomDate(start, end)}`)}>Change Content</button>
+            <NasaCard imgUrl={nasaData.url} data={nasaData} />
+        </div>
+
+    )
+}
+
+export default CardContainer;
