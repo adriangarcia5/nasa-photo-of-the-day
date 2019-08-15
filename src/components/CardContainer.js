69a837d1762f0d18cfd033b34c637729202eb193
Adrian-Garcia <agarc5@hotmail.com>
2019-08-15 17:28:42 -0500

    added 5 new things to the npm, started refactoring

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -4,15 +4,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import NasaCard from "./NasaCard.js";
 import randomDate from "./randomDate.js";
+import { Container, Button, Icon, Menu } from "semantic-ui-react";
 
 function CardContainer() {
     // NASA KEY: https://api.nasa.gov/planetary/apod?api_key=JmvQUr9DIoMy0TsbX9mx0ZbTpbVnMDiWhUdFBCIg
     const [nasaData, setNasaData] = useState([])
-    const [date, setDate] = useState('2012-3-14')
+    const [date, setDate] = useState('2019-07-18')
     let start = new Date(2001, 0, 1)
     let end = new Date()
-    
-    useEffect(() => {
+
+     useEffect(() => {
         axios
             .get(`https://api.nasa.gov/planetary/apod?api_key=JmvQUr9DIoMy0TsbX9mx0ZbTpbVnMDiWhUdFBCIg&date=${date}`)
             .then(response => {
@@ -25,13 +26,18 @@ function CardContainer() {
             })
     }, [date])
 
-    return (
-        <div className="card-container">
-            <button onClick={() => setDate(`${randomDate(start, end)}`)}>Change Content</button>
-            <NasaCard imgUrl={nasaData.url} data={nasaData} />
-        </div>
-
+     return (
+        <Container>
+            <Menu>
+                <Menu.Item position="right" name="Choose Date">CONTENT CHANGER
+                </Menu.Item>
+                <Menu.Item position="left" name="Random Content" onClick={() => setDate(`${randomDate(start, end)}`)}>Choose Date</Menu.Item>
+            </Menu>
+            <Container className="card-container">
+                <NasaCard data={nasaData}></NasaCard>
+            </Container>
+        </Container>
     )
 }
 
-export default CardContainer;
+ export default CardContainer; 
